c47d056db482e087c142f8f4d0bedef1c29937f1
musawakiliML <musawakiliml@hotmail.com>
2022-09-17 14:30:28 +0100

    Update

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-alert("Hello, Javascript!!.")
-alert("I love Eloquent Javascript!!")
+//alert("Hello, Javascript!!.")
+//alert("I love Eloquent Javascript!!")
+
+
+// variables
+var myName = "Net Ninja";
+
+alert("Name:" + myName);
+//console.log("Name:" + myName);
+
