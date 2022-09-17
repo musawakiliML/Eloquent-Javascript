10fe5215aa3eeaeda1aeda90786798c3e4048aab
musawakiliML <musawakiliml@hotmail.com>
2022-09-17 14:36:59 +0100

    Maths operators

--- index.js
+++ index.js
@@ -5,6 +5,28 @@
 // variables
 var myName = "Net Ninja";
 
-alert("Name:" + myName);
+//alert("Name:" + myName);
 //console.log("Name:" + myName);
 
+
+// maths operators
+var num1 = 12;
+var num2 = 13;
+
+var sum;
+var mult;
+var div;
+var sub;
+
+var average;
+
+sum = num1 + num2;
+mult = num1 * num2;
+div = num1 / num2;
+sub = num1 - num2;
+
+
+console.log(num1 + " + " + num2 + " = " + sum);
+console.log(num1 + " - " + num2 + " = " + sub);
+console.log(num1 + " * " + num2 + " = " + mult);
+console.log(num1 + " / " + num2 + " = " + div);
