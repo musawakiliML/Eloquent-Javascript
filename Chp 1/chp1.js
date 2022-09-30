2095bdb1185775b5d5a6fb7888d7df8ded940e00
musawakiliML <musawakiliml@hotmail.com>
2022-09-30 14:51:30 +0100

    updated chp1.js

--- Chp 1/chp1.js
+++ Chp 1/chp1.js
@@ -4,12 +4,28 @@
 
 
 // Numbers
-console.log(12) // integer number
-console.log(12.9) // float number
+console.log("Number Types in Javascript")
+console.log("Integer Number:",12) // integer number
+console.log("Float Number:",12.9) // float number
 
 
 // Arithmetic 
-console.log(12+ 12) // addition
-console.log("") //
+console.log("Arithmetic In Javascript")
+console.log("12 + 12 =", 12 + 12) // addition
+console.log("12 - 12 =", 12-12) // subtraction
+console.log("12 / 11 =",12/11 ) // division
+console.log("12 * 10 =",12*10) // multiplication
+console.log("12 ** 2 =", 12**2) // exponent
+console.log("12 % 3 =", 12%3) // modulus
+
+// Special Numbers
+console.log("Special Numbers in Javascript")
+console.log(Infinity)
+console.log(-Infinity)
+console.log(NaN)
+
+
+// Strings
+console.log("Strings in Javascript")
 console.log("A newline character is written like \"\\n\".")
 console.log(`The square of 12 is ${12 ** 2}`)
