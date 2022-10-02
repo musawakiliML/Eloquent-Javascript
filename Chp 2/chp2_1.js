f3fb4dc5c97edaa2d43e2726634091a567c5d5a7
musawakiliML <musawakiliml@hotmail.com>
2022-10-02 02:07:27 +0100

    Added a small program using If

--- Chp 2/chp2_1.js
+++ Chp 2/chp2_1.js
@@ -2,3 +2,15 @@
 Program Structure */
 // import prompt for command line
 const prompt = require("prompt-sync")();
+
+// number range
+let number = Number(prompt('Enter a Number:'));
+if (number < 10){
+    console.log("Small");
+}
+else if (number < 100){
+    console.log("Medium");
+}
+else {
+    console.log("Large");
+}
