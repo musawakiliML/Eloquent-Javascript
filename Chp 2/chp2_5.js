e0fa65ec7c31a8c5201a33084533b4f233d56d7d
musawakiliML <musawakiliml@hotmail.com>
2022-10-02 12:50:40 +0100

    testing break statement

--- Chp 2/chp2_5.js
+++ Chp 2/chp2_5.js
@@ -9,3 +9,12 @@ let range = Number(prompt("Enter a range:"))
 for (number = 0; number < range; number++){
     console.log("Hi!, For Loops...")
 }
+
+
+// break statement
+for (let current = 20; ; current = current + 1){
+    if (current % 7 == 0){
+        console.log(current);
+        break;
+    }
+}
