2ffee6992ce0bcfbc870b39151c8a061247a2c58
musawakiliML <musawakiliml@hotmail.com>
2022-10-02 01:10:03 +0100

    Updated variables

--- Chp 2/chp2.js
+++ Chp 2/chp2.js
@@ -4,3 +4,20 @@ Program Structure */
 
 // Expressions and statements
 console.log("This is a statement!!");
+
+// Bindings or Variables
+
+let value = 12;
+console.log(value);
+
+
+let debt = 140;
+debt = debt - 35;
+console.log(debt);
+
+
+let name;
+console.log(name);
+
+let num1 = 1, num2 = 12;
+console.log(num1 + num2);
