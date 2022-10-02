d3900bde6af2413c1f32bc1069cb6c93cdacbd0b
musawakiliML <musawakiliml@hotmail.com>
2022-10-02 02:10:21 +0100

    while loop program

--- Chp 2/chp2_2.js
+++ Chp 2/chp2_2.js
@@ -2,3 +2,12 @@
 Program Structure */
 // import prompt for command line
 const prompt = require("prompt-sync")();
+
+// while loop
+
+let number = 0;
+
+while (number <= 12){
+    console.log(number);
+    number = number + 2;
+}
