500866b84c3df9918f3b136ddc6b1830d00729d1
musawakiliML <musawakiliml@hotmail.com>
2022-10-07 14:40:20 +0100

    Update with multiple arguments functions

--- Chp 3/chp3_1.js
+++ Chp 3/chp3_1.js
@@ -109,3 +109,12 @@ console.log(square3(4, true, "hedgehog"));
 
 //console.log(x);
 //var x = 90;
+
+// minus function
+function minus(a, b){
+    if (b === undefined) return -a;
+    else return a - b;
+}
+
+console.log(minus(10))
+console.log(minus(10, 200))
