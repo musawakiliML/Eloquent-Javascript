096c0eba34ff9385ed11d4be70f5c9ae98139678
musawakiliML <musawakiliml@hotmail.com>
2022-10-07 17:35:28 +0100

    Added closure section

--- Chp 3/chp3_1.js
+++ Chp 3/chp3_1.js
@@ -130,3 +130,17 @@ function power_new(base, exponent = 2){
 }
 
 console.log(power_new(7));
+
+
+// Closure
+
+function wrapValue(n){
+    let local = n;
+    return () => local;
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+
+console.log(wrap1());
+console.log(wrap2());
